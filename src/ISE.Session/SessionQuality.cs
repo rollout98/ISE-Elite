d93209b659c7046fb337a1343e73452f2b6d7f2e
rollout98 <devonlewis@yahoo.com>
2026-08-03 16:37:49 -0500

    Add session quality model

---
+++ src/ISE.Session/SessionQuality.cs
@@ -0,0 +1,11 @@
+namespace ISE.Session;
+
+/// <summary>Describes the expected opportunity quality of the current time window.</summary>
+public enum SessionQuality
+{
+    Closed,
+    Low,
+    Normal,
+    High,
+    Prime
+}
