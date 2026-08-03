84d25ebd1ffa9d5ef13393e9d086b4b7d0e52598
rollout98 <devonlewis@yahoo.com>
2026-08-03 16:37:59 -0500

    Add trading calendar status

---
+++ src/ISE.Session/TradingCalendarStatus.cs
@@ -0,0 +1,9 @@
+namespace ISE.Session;
+
+/// <summary>Describes exchange-calendar availability for the evaluated trading day.</summary>
+public enum TradingCalendarStatus
+{
+    Normal,
+    HolidayClosed,
+    EarlyClose
+}
