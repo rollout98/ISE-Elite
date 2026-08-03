d70e7df00e45bf76b10314cd51341462353a0a9e
rollout98 <devonlewis@yahoo.com>
2026-08-03 14:32:52 -0500

    Add strategy identifiers

---
+++ src/ISE.Strategy/StrategyId.cs
@@ -0,0 +1,10 @@
+namespace ISE.Strategy;
+
+/// <summary>Identifies a supported strategy playbook.</summary>
+public enum StrategyId
+{
+    /// <summary>New York open reversal playbook.</summary>
+    NewYorkOpenReversal,
+    /// <summary>New York continuation playbook.</summary>
+    NewYorkContinuation
+}
