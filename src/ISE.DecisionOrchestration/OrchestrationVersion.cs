ae756aa4f035c8d9fb75f911fdf254e60d833944
rollout98 <devonlewis@yahoo.com>
2026-08-03 15:57:56 -0500

    Add orchestration version metadata

---
+++ src/ISE.DecisionOrchestration/OrchestrationVersion.cs
@@ -0,0 +1,8 @@
+namespace ISE.DecisionOrchestration;
+
+/// <summary>Provides Decision Orchestration component version metadata.</summary>
+public static class OrchestrationVersion
+{
+    /// <summary>Gets the current component version.</summary>
+    public const string Current = "0.1.0";
+}
