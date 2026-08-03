ff6514192f3ed73a2bfa61fadaf48be99e6c75c0
rollout98 <devonlewis@yahoo.com>
2026-08-03 15:24:30 -0500

    Add execution order sides

---
+++ src/ISE.Execution/ExecutionSide.cs
@@ -0,0 +1,10 @@
+namespace ISE.Execution;
+
+/// <summary>Identifies whether an order buys or sells contracts.</summary>
+public enum ExecutionSide
+{
+    /// <summary>Buys contracts.</summary>
+    Buy,
+    /// <summary>Sells contracts.</summary>
+    Sell
+}
